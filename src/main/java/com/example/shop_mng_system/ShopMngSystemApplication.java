45546993db9078f64850868a11b4078ca4ce6014
andicaka9@gmail.com <andicaka9@gmail.com>
2024-05-03 18:39:30 +0300

    connect to db

---
+++ src/main/java/com/example/shop_mng_system/ShopMngSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.shop_mng_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMngSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopMngSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shop_mng_system/ShopMngSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shop_mng_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMngSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
